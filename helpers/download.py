2b73b943553808604bfaffe07b6934e5661c0824
Anthony Izzo <anthonyizzo@anthonys-air.lan>
2023-11-08 00:24:31 -0800

    revamping download by appending CRC32 values

--- helpers/download.py
+++ helpers/download.py
@@ -1,26 +1,38 @@
 import os
 import urllib.request
 import logging
+import argparse
+import binascii
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--output_dir", default="../lists", help="Directory to save the files")
+args = parser.parse_args()
+
 # Create the directory if it doesn't exist
-os.makedirs('../lists', exist_ok=True)
+os.makedirs(args.output_dir, exist_ok=True)
 
 # List of files to download
 files = [
-    ('https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/domains.txt', '../lists/1hosts_lite.txt'),
-    ('https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt', '../lists/mullvad_adblock.txt'),
-    ('https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt', '../lists/mullvad_privacy.txt'),
+    ('https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/domains.txt', '1hosts_lite.txt'),
+    ('https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt', 'mullvad_adblock.txt'),
+    ('https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt', 'mullvad_privacy.txt'),
 ]
 
 # Download and rename files
 for url, filename in files:
     try:
         logging.info(f"Starting download of {url} to {filename}")
-        urllib.request.urlretrieve(url, filename)
-        logging.info(f"Successfully downloaded {url} to {filename}")
+        response = urllib.request.urlopen(url)
+        data = response.read()      # a `bytes` object
+        crc32_code = binascii.crc32(data) & 0xffffffff  # compute CRC32
+        filename_without_ext, extension = os.path.splitext(filename)
+        new_filename = os.path.join(args.output_dir, f"{filename_without_ext}-{format(crc32_code, '08x')}{extension}")
+        with open(new_filename, 'wb') as f:
+            f.write(data)
+        logging.info(f"Successfully downloaded {url} to {new_filename}")
     except Exception as e:
         logging.error(f"Error downloading {url} to {filename}: {e}")
-
--- helpers/processor.py
+++ helpers/processor.py
@@ -38,12 +38,15 @@ def start(files: List[str], output_file: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Read files and apply filtering')
-    parser.add_argument('files', metavar='F', type=str, nargs='*', help='an input file for processing')
+    parser.add_argument('files', metavar='F', type=str, nargs='*', help='an input file or directory for processing')
     parser.add_argument('--out', dest='output_file', default='../output.txt', help='the output file (default: output.txt)')
     args = parser.parse_args()
 
-    # If no files are provided as command line arguments, use all .txt files in ../lists
-    if not args.files:
-        args.files = glob.glob('../lists/*.txt')
+    files = []
+    for path in args.files:
+        if os.path.isdir(path):
+            files.extend(glob.glob(path + '/*.txt'))
+        else:
+            files.append(path)
 
-    start(args.files, args.output_file)
+    start(files, args.output_file)
